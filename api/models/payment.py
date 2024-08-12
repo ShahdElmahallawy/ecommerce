8117c28d184a46031689fd33860114f89e8d8215
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2024-08-13 01:51:39 +0300

    feat: add payment model

---
+++ api/models/payment.py
@@ -0,0 +1,22 @@
+from django.db import models
+from . import User
+
+class Payment(models.Model):
+    """
+    Model representing a payment.
+    Attributes:
+        user: The user who made the payment.
+        pan: The PAN (Primary Account Number) of the payment.
+        bank_name: The name of the bank associated with the payment.
+        expiry_date: The expiry date of the payment.
+        cvv: The CVV (Card Verification Value) of the payment.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pan = models.CharField(max_length=16, unique=True)
+    bank_name = models.CharField(max_length=255)
+    expiry_date = models.DateField()
+    cvv = models.CharField(max_length=3)
+
+    def __str__(self):
+        """Returns a string representation of the payment."""
+        return f"{self.user.username} - bank: {self.bank_name}"
