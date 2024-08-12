0d1b433be739517aab244cc7f69712cfbd835cbe
Shahd <shahd.kh@aucegypt.edu>
2024-08-12 18:33:09 +0300

    updated models (cart, cart_items)

--- api/models/cart.py
+++ api/models/cart.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.utils import timezone
+# from api.models.user import User
+from api.models.cart_items import CartItems
 from django.contrib.auth import get_user_model
+
 
 
 User = get_user_model()
@@ -9,7 +13,11 @@ class Cart(models.Model):
 
     Fields:
     - user: user of cart (one to one relation)
+    - items: items of cart (one to many relation)
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    items = models.ForeignKey(CartItems, on_delete=models.CASCADE)
+
     def __str__(self):
         return "user: "+str(self.user) + ", items: " + str(self.items)
+        
