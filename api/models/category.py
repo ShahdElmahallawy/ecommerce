5d02b5bc0a1247c9fbb18f2eb14224bb15de34e9
fadymalak <fady.malak.awad@gmail.com>
2024-08-14 11:51:39 +0300

    Revert "order/order_item/category models"

--- api/models/category.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    featred_product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name='category')
-
-    def __str__(self):
-        return self.name
